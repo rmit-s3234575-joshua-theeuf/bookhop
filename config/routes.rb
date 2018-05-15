bd0964471dec71af50ed6246e89020e398e59971
Ubuntu <ubuntu@ip-172-31-9-31.ap-southeast-2.compute.internal>
2018-05-15 23:12:30 +0000

    fixed routes to support admin and customised controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :indices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-      devise_for :users, controllers: {
+      devise_for :admins, controllers: {
         sessions: 'users/sessions',
 	registration: 'users/registration'
       }
