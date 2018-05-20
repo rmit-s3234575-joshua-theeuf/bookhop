85ab9e120362e5998c576b1d2b619c2868c16d21
deepozzzie <s3234575@student.rmit.edu.au>
2018-05-20 12:15:30 +1000

    update rake routes to represent admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :indices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
       devise_for :admins, controllers: {
-        sessions: 'users/sessions',
-	registration: 'users/registration'
+        sessions: 'admin/sessions',
+	      registration: 'admin/registration'
       }
 
   root "indices#index"
