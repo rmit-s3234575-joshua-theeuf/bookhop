db4bcc6c3fb9b56cbe7fd6eb37be0642177c5c55
deepozzzie <s3234575@student.rmit.edu.au>
2018-05-20 12:33:22 +1000

    fix the application controller, and devise models to support creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def devise_parameter_sanitizer
+    if resource_class == Admin
+      Admin::ParameterSanitizer.new(Admin, :admin, params)
+    else
+      super # Use the default one
+    end
+  end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,3 +4,10 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
+class Admin::ParameterSanitizer < Devise::ParameterSanitizer
+  def initialize(*)
+    super
+    permit(:sign_up, keys: [:username, :email, :firstname, :lastname, :phonenumber])
+  end
+end
